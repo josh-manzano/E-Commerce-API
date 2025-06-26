b12d94ce50d703b0b20442a4a2d454c1e82a0958
Josh M <manzojosh27@gmail.com>
2025-06-25 19:12:59 -0700

    finished MySqlCategoryDao

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -25,39 +25,61 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         // get all categories
         String sql = "SELECT * FROM categories;";
         try{
-            return jdbcTemplate.query(sql, (rs, 1) -> mapRow(rs));
+            return jdbcTemplate.query(sql, (rs, numRow) -> mapRow(rs));
         }catch (DataAccessException e){
             e.printStackTrace();
             return new ArrayList<>();
         }
-
-        return null;
     }
 
     @Override
     public Category getById(int categoryId)
     {
         // get category by id
-        return null;
+        String sql = "SELECT * FROM categories WHERE category_id = ?;";
+        try{
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> mapRow(rs), categoryId);
+        }catch (DataAccessException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
-    public Category create(Category category)
-    {
+    public Category create(Category category) {
         // create a new category
-        return null;
+        String sql = "INSERT INTO categories (name, description) VALUES (?, ?);";
+        try {
+            jdbcTemplate.update(sql, category.getName(), category.getDescription());
+            return category;
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void update(int categoryId, Category category)
     {
         // update category
+        String sql = "UPDATE categories SET name = ?, description = ? WHERE category_id = ?;";
+        try{
+            jdbcTemplate.update(sql, category.getName(), category.getDescription(), categoryId);
+        }catch (DataAccessException e){
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void delete(int categoryId)
     {
         // delete category
+        String sql = "DELETE FROM categories WHERE category_id = ?;";
+        try{
+            jdbcTemplate.update(sql, categoryId);
+        }catch (DataAccessException e){
+            e.printStackTrace();
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
